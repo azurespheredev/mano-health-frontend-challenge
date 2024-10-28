fb251438064f19f8acece19a4cd16a86623ab611
BlitzSprinter <dustin.jason.lee@gmail.com>
2024-10-28 18:14:50 -0500

    backend: allow all cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+// Allow CORS
+app.use("*", cors());
 
 serve({ fetch: app.fetch, port: 8080 });
 console.log("Server is running on http://localhost:8080");
